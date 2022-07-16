a4be82caa5ef934cab8ce351d8b9bc54e985657b
IDotan <itai@lukio.pro>
2022-07-16 15:46:38 +0300

    move the update to separate file

--- lukio-theme/functions.php
+++ lukio-theme/functions.php
@@ -7,3 +7,4 @@ if (!defined('ABSPATH')) {
 require_once __DIR__ . '/inc/lukio_helpers.php';
 require_once __DIR__ . '/inc/lukio_theme_setup.php';
 require_once __DIR__ . '/inc/lukio_svg_support.php';
+require_once __DIR__ . '/inc/lukio_theme_update.php';
