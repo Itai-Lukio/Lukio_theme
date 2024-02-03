33c7ab44640297d6a3b3183fd2588609553c4ccf
Itai-Lukio <itai@lukio.pro>
2024-02-03 18:15:05 +0200

    add single.php and basic css for it

---
+++ lukio-theme/single.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Used to show single post content
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ */
+
+// Exit if accessed directly.
+defined('ABSPATH') || exit;
+
+do_action('wp_enqueue_scripts');
+
+get_header();
+
+// the div#base_single is used for basic css for h1-6 and p, it its NOT a must keep
+?>
+<div id="base_single">
+    <h1><?php echo the_title(); ?></h1>
+    <?php the_content();  ?>
+</div>
+
+<?php
+
+get_footer();
