3bd246b55b9205c2606ddc111d6aded28069b390
Itai-Lukio <itai@lukio.pro>
2024-11-10 17:22:47 +0200

    fix the generic page and single and their css

--- lukio-theme/page.php
+++ lukio-theme/page.php
@@ -12,12 +12,14 @@ defined('ABSPATH') || exit;
 
 do_action('wp_enqueue_scripts');
 
-ob_start();
-the_content();
-$content = ob_get_clean();
-
 get_header();
 
-echo $content;
+// the div#base_single is used for basic css for h1-6, p, ul and ol, it its NOT a must keep
+?>
+<div id="base_single">
+    <?php the_content(); ?>
+</div>
+
+<?php
 
 get_footer();
--- lukio-theme/single.php
+++ lukio-theme/single.php
@@ -14,7 +14,7 @@ do_action('wp_enqueue_scripts');
 
 get_header();
 
-// the div#base_single is used for basic css for h1-6 and p, it its NOT a must keep
+// the div#base_single is used for basic css for h1-6, p, ul and ol, it its NOT a must keep
 ?>
 <div id="base_single">
     <h1><?php echo the_title(); ?></h1>
