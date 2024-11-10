b3429426d194792f94c6611c9317d3c37c7a12bc
Itai-Lukio <itai@lukio.pro>
2024-11-11 00:35:30 +0200

    fix page.php and add simple content template

---
+++ lukio-theme/templates/simple_content.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Template Name: Simple content
+ */
+
+// Exit if accessed directly.
+defined('ABSPATH') || exit;
+
+do_action('wp_enqueue_scripts');
+
+get_header();
+
+// the div#base_single is used for basic css for h1-6, p, ul and ol, it its NOT a must keep
+?>
+<div id="base_single">
+    <?php the_content();  ?>
+</div>
+
+<?php
+
+get_footer();
